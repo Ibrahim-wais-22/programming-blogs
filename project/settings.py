ff2daef2d7758c2674ecca1f32cb2b1e71189b6f
Ibrahim-wais-22 <aabbdd714@gmail.com>
2021-12-30 11:43:32 +0200

    secend commit

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v1k@nr#(yhqgi1(kalz#sy23h7)sirxh1p5r$sqjxm2jks4tg7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogsprogrammer.herokuapp.com','127.0.0.1']
 
 
 # Application definition
